80970bb0106e2d15211d55cf28980d2fac86d1a6
takahiro-inoway <takahiro.inoway@gmail.com>
2022-04-30 15:42:15 +0900

    [add]220430_a_first

--- src/laravel/routes/web.php
+++ src/laravel/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Route::post('/', 'App\Http\Controllers\PagesController@save');
